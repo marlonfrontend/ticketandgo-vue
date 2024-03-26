5bc1ea7fb5dd8a71c1b9dc5ff7dd88d39dfda18c
Marlon Felix <marlonsantos@kirvano.com>
2024-03-26 18:31:04 -0300

    chore: add component brand

---
+++ src/components/ui/AppBrand.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center">
+    <RouterLink to="/" class="inline-block">
+      <img src="@/assets/img/logo.svg" alt="" class="block w-[180px]" />
+    </RouterLink>
+  </div>
+</template>
