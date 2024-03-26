db7883429d3ea681dcd72b04111e6a5b8b966a13
Marlon Felix <marlonsantos@kirvano.com>
2024-03-26 18:46:41 -0300

    chore: create image component

---
+++ src/components/ui/AppImage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import img from '@/assets/img/no-image.jpg'
+defineProps({
+  src: {
+    type: String
+  },
+  alt: {
+    type: String
+  }
+})
+
+const errorImage = (e) => {
+  e.target.src = img
+}
+</script>
+
+<template>
+  <img :src="src" v-lazy="{ src: src }" :alt="alt" @error="errorImage" />
+</template>
